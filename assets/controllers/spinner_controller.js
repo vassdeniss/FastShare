85e6288a68b6b4af93cf3529d4e149e7b2444859
Denis Vasilev <vassdeniss@gmail.com>
2024-12-05 22:59:29 +0200

    feat: added spinner

---
+++ assets/controllers/spinner_controller.js
@@ -0,0 +1,9 @@
+﻿import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["loader"];
+
+    show(event) {
+        this.loaderTarget.classList.remove("hidden");
+    }
+}
